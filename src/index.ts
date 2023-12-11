5fc9039d6f36c18171e4d5bdbe4e082da5659742
Sergey <mail@vasya.com>
2023-12-12 01:42:50 +0300

    fixed bugs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import "./styles/styles.scss";
-import 'emoji-picker-element'
+import "emoji-picker-element";
 
 import { store } from "./modules/redux/store";
 
@@ -9,28 +9,29 @@ import { Message } from "./modules/types";
 import { insertMessage } from "./modules/firebaseAPI";
 
 window.onload = function () {
-	const inputMessage = document.querySelector(".message-text") as HTMLTextAreaElement
+  const inputMessage = document.querySelector(
+    ".message-text",
+  ) as HTMLTextAreaElement;
   const sendBtn = document.querySelector("#submit-btn");
   const messages = document.querySelector(".messages-list") as HTMLElement;
   const emojiBtn = document.querySelector(".emoji-btn");
-  const emojiPicker = document.querySelector("#emoji-picker")
+  const emojiPicker = document.querySelector("#emoji-picker");
 
   document.addEventListener("load", store.dispatch(fetchMessages() as any));
 
   if (emojiBtn && emojiPicker) {
-		emojiPicker.addEventListener('emoji-click', (event: any)=> {
-			inputMessage.value += event.detail.unicode
-		} )
+    emojiPicker.addEventListener("emoji-click", (event: any) => {
+      inputMessage.value += event.detail.unicode;
+    });
 
-		emojiBtn.addEventListener("click", function() {
-			if (emojiPicker.classList.contains('hidden')) {
-				emojiPicker.classList.remove('hidden');
-			}
-			else {
-				emojiPicker.classList.add('hidden')
-			}
-		})
-	}
+    emojiBtn.addEventListener("click", function () {
+      if (emojiPicker.classList.contains("hidden")) {
+        emojiPicker.classList.remove("hidden");
+      } else {
+        emojiPicker.classList.add("hidden");
+      }
+    });
+  }
 
   if (messages) {
     store.subscribe(() => renderMessages(messages));
